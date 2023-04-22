0a15641b87205d6ed7e208da481700b73d015a05
Victor F. Santos <victor.gaard@gmail.com>
2023-04-23 01:41:02 +0200

    fix: change in environments url

--- environments/environments.ts
+++ environments/environments.ts
@@ -1,5 +1,5 @@
 const development = 'http://localhost:3000';
-const production = 'https://victor.sawthat.band';
+const production = 'https://www.sawthat.band';
 
 export const API_URL =
   process.env.NODE_ENV === 'development' ? development : production;
