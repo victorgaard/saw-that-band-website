861e62af501d9c7ab68a236fd6cec9497655e5f9
Victor F. Santos <victor.gaard@gmail.com>
2023-04-23 13:54:08 +0200

    feat: add favico

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-zinc-900">
       <body className={font.className}>{children}</body>
       <GoogleAnalyticsWrapper />
     </html>
