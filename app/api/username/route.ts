1c38f446938849cc7913d83fae63599d5d5459e6
Victor F. Santos <victor.gaard@gmail.com>
2023-04-25 22:07:36 +0200

    feat: add sendgrid

--- app/api/username/route.ts
+++ app/api/username/route.ts
@@ -1,4 +1,5 @@
 import supabase from "@/app/utils/supabase";
+import sendgrid from "@sendgrid/mail";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
@@ -21,6 +22,25 @@ export async function POST(request: Request) {
   const { data, error } = await supabase
     .from("Waitlist")
     .insert([{ email, username }]);
+
+  sendgrid.setApiKey(process.env.SENDGRID_API_KEY!);
+
+  const options = {
+    to: "victor.gaard@gmail.com",
+    from: "victor@sawthat.band",
+    subject: "Sending with SendGrid is Fun",
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
+
+  sendgrid
+    .send(options)
+    .then(() => {
+      console.log("Email sent");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 
   return NextResponse.json({ data, error });
 }
