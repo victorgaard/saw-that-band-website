da712c4decab87818fe5190bac24755fd5f24b44
Victor F. Santos <victor.gaard@gmail.com>
2023-04-26 09:56:32 +0200

    chore: adjusting email

--- app/api/username/route.ts
+++ app/api/username/route.ts
@@ -2,18 +2,6 @@ import supabase from "@/app/utils/supabase";
 import sendgrid from "@sendgrid/mail";
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const username = searchParams.get("username");
-
-  const { data, error } = await supabase
-    .from("Waitlist")
-    .select()
-    .eq("username", username);
-
-  return NextResponse.json(data);
-}
-
 export async function POST(request: Request) {
   const { searchParams } = new URL(request.url);
   const email = searchParams.get("email");
@@ -23,24 +11,26 @@ export async function POST(request: Request) {
     .from("Waitlist")
     .insert([{ email, username }]);
 
-  sendgrid.setApiKey(process.env.SENDGRID_API_KEY!);
+  if (!error && email && username) {
+    sendgrid.setApiKey(process.env.SENDGRID_API_KEY!);
 
-  const options = {
-    to: "victor.gaard@gmail.com",
-    from: "victor@sawthat.band",
-    subject: "Sending with SendGrid is Fun",
-    text: "and easy to do anywhere, even with Node.js",
-    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
-  };
+    const options = {
+      to: email,
+      from: process.env.SENDGRID_FROM_EMAIL!,
+      subject: `Your username ${username} is reserved 🤘`,
+      text: "Thanks for joining the waitlist of Saw that Band. Once it's launched you will receive an email to complete your registration.",
+      html: "<strong>Thanks for joining the waitlist of Saw that Band. Once it's launched you will receive an email to complete your registration.</strong>",
+    };
 
-  sendgrid
-    .send(options)
-    .then(() => {
-      console.log("Email sent");
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+    sendgrid
+      .send(options)
+      .then(() => {
+        console.log("Email sent");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 
   return NextResponse.json({ data, error });
 }
