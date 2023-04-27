47558795f1a1b25d74fd56e4851b481f93db512a
Victor F. Santos <victor.gaard@gmail.com>
2023-04-27 18:45:42 +0200

    chore: sendgrid

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,8 +1,8 @@
 import supabase from "@/app/utils/supabase";
-import sendgrid from "@sendgrid/mail";
 import { NextResponse } from "next/server";
 import { render } from "@react-email/render";
 import WaitlistWelcomeEmail from "@/app/email/WaitlistWelcomeEmail";
+import sendgrid from "@/app/utils/sendgrid";
 
 export async function POST(request: Request) {
   const { searchParams } = new URL(request.url);
@@ -14,9 +14,7 @@ export async function POST(request: Request) {
     .insert([{ email, username }]);
 
   if (!error && email && username) {
-    sendgrid.setApiKey(process.env.SENDGRID_API_KEY!);
     const html = render(WaitlistWelcomeEmail({ username }));
-
     const options = {
       to: email,
       from: process.env.SENDGRID_FROM_EMAIL!,
@@ -24,14 +22,12 @@ export async function POST(request: Request) {
       text: `Thanks for joining our waitlist. Your username ${username} is now reserved on Saw that Band. We will notify you via email as soon as the app is launched to complete the registration.`,
       html,
     };
-
     sendgrid
       .send(options)
       .then(() => {
         console.log("Email sent");
       })
       .catch((error) => {
-        throw new Error(`Couldn't send email: ${error}`);
         console.error(error);
       });
   }
