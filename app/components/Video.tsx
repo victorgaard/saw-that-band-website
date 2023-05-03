1d136f328ac40ff10c2e46b1954e8f19f8fdd48d
Victor F. Santos <victor.gaard@gmail.com>
2023-05-03 14:37:36 +0200

    chore: display video only if not on mobile

--- app/components/Video.tsx
+++ app/components/Video.tsx
@@ -1,20 +1,24 @@
 "use client";
 
-import { UAParser } from "ua-parser-js";
+import { useEffect, useState } from "react";
 
 function Video() {
-  const userAgent = typeof window !== "undefined" && navigator.userAgent || undefined;
-  const parser = new UAParser(userAgent);
-  const device = parser.getDevice().type
+  const [isMobile, setIsMobile] = useState(false);
 
-  if (device) return null;
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setIsMobile(window.innerWidth < 600)
+    }
+  }, [])
+
+  if (isMobile) return null;
 
   return (
     <video
       autoPlay
       muted
       loop
-      className="hidden sm:block -mt-8 -right-4 absolute z-0 pointer-events-none [mask-image:linear-gradient(to_top,transparent_5%,#18181B_25%)] opacity-80 object-cover 3xl:w-[calc(100%-500px)]"
+      className="-mt-8 -right-4 absolute z-0 pointer-events-none [mask-image:linear-gradient(to_top,transparent_5%,#18181B_25%)] opacity-80 object-cover 3xl:w-[calc(100%-500px)]"
     >
       <source
         src="https://uploadthing.com/f/30f015ae-6bc9-4854-9842-4e6006e0bf9d_demo.mp4"
