d70e02353ea8800b69da8227bc845aed07fa8efe
Victor F. Santos <victor.gaard@gmail.com>
2023-05-04 12:25:44 +0200

    chore: reduce video size

--- app/components/Video.tsx
+++ app/components/Video.tsx
@@ -21,7 +21,7 @@ function Video() {
       className="-mt-8 -right-4 absolute z-0 pointer-events-none [mask-image:linear-gradient(to_top,transparent_5%,#18181B_25%)] opacity-80 object-cover 3xl:w-[calc(100%-500px)]"
     >
       <source
-        src="https://uploadthing.com/f/30f015ae-6bc9-4854-9842-4e6006e0bf9d_demo.mp4"
+        src="https://uploadthing.com/f/afd1e871-a354-4b9c-95a4-301111abe06a_demo%20quick%2030.mp4"
         type="video/mp4"
       />
       Your browser does not support the video tag.
