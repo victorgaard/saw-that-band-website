d96625e8a6425fc782f577b28e9800cf8bf641bf
Victor F. Santos <victor.gaard@gmail.com>
2024-12-01 21:30:51 -0300

    chore: add login button to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -17,28 +17,41 @@ export default async function Home() {
       </div>
       <main className="flex relative">
         <div className="flex items-center px-4 sm:px-12 bg-zinc-800/50 border-r border-r-zinc-500/30 shadow-2xl w-full sm:w-[500px] backdrop-blur-lg text-white sm:ml-20 gap-12 h-screen">
-          <div className="flex flex-col gap-10">
-            <h1 className="text-3xl leading-8 sm:text-4xl sm:leading-9 font-bold">
+          <div className="flex flex-col">
+            <h1 className="text-3xl leading-8 sm:text-4xl sm:leading-9 font-bold pb-10">
               Turn your concert memories into an immersive and shareable
               experience
             </h1>
-            <a
-              href="https://app.sawthat.band/signup?utm_source=website"
-              className="bg-gradient-to-br flex items-center justify-center gap-1 from-emerald-400 to-green-400 hover:from-green-400 hover:to-green-400 text-zinc-900 text-sm font-semibold p-4 rounded-lg"
-            >
-              Create your profile now <span className="text-lg">🤘</span>
-            </a>
-            <a
-              href="https://victor.sawthat.band"
-              target="_blank"
-              className="text-sm self-center font-medium flex items-center gap-2 group"
-              rel="noreferrer"
-            >
-              <span className="group-hover:underline decoration-2 underline-offset-8 decoration-emerald-400">
-                See a live example
-              </span>
-              <ArrowRightIcon className="h-4 w-4 group-hover:ml-2 transition-all group-hover:text-emerald-400" />
-            </a>
+            <div className="space-y-4">
+              <a
+                href="https://app.sawthat.band/signup?utm_source=website"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bg-gradient-to-br flex items-center justify-center gap-1 from-emerald-400 to-green-400 hover:from-green-400 hover:to-green-400 text-zinc-900 text-sm font-semibold p-4 rounded-lg"
+              >
+                Create your profile now <span className="text-lg">🤘</span>
+              </a>
+
+              <a
+                href="https://app.sawthat.band/login"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center justify-center rounded-lg border border-zinc-700/60 p-4 text-sm font-medium text-zinc-300 shadow-sm transition-colors duration-75 hover:border-zinc-600 hover:bg-zinc-700/50 hover:text-white"
+              >
+                Login
+              </a>
+              <a
+                href="https://victor.sawthat.band"
+                target="_blank"
+                className="text-sm justify-center font-medium flex items-center gap-2 p-4 group"
+                rel="noopener noreferrer"
+              >
+                <span className="group-hover:underline decoration-2 underline-offset-8 decoration-emerald-400">
+                  See a live example
+                </span>
+                <ArrowRightIcon className="h-4 w-4 group-hover:ml-2 transition-all group-hover:text-emerald-400" />
+              </a>
+            </div>
           </div>
           <div className="absolute bottom-6 left-12 flex items-center gap-8 text-sm text-zinc-400">
             <a
@@ -50,9 +63,9 @@ export default async function Home() {
               Privacy policy
             </a>
             <a
-               href="/terms-and-conditions/index.html"
-               target="_blank"
-               rel="noreferrer"
+              href="/terms-and-conditions/index.html"
+              target="_blank"
+              rel="noreferrer"
               className="hover:text-white hover:underline underline-offset-2"
             >
               Terms and conditions
